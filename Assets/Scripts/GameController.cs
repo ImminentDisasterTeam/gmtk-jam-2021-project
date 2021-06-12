0df12124d8ab60d103911630bdfed2717a2fe914
Ekaterina <kitekate2000cat@gmail.com>
2021-06-12 21:28:34 +0300

    attempt to fix number spawn

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,7 +6,7 @@ public class GameController : MonoBehaviour
 {
     int minValue = 1;
     int maxValue = 20;
-    float numberRadius = 8f;
+    Vector2 numberRadius = new Vector2(2f, 2f);
     Player player;
     [SerializeField] GameObject playerObject;
     [SerializeField] GameObject numberObject;
@@ -24,6 +24,8 @@ public class GameController : MonoBehaviour
     void SpawnNewNumber()
     {
         Vector3 spawnPoint = GetRandomPoint();
+        if (spawnPoint == Vector3.zero)
+            return;
         Number number = Instantiate(numberObject, spawnPoint, Quaternion.identity).GetComponent<Number>();
         number.Initiate(Random.Range(minValue, maxValue));
     }
@@ -34,13 +36,20 @@ public class GameController : MonoBehaviour
         float y = Random.Range(-5f, 5f);
         Vector3 vector = new Vector3(x, y);
 
-        while (Physics2D.OverlapCircle(vector, numberRadius, 0) != null)
+        RaycastHit2D hit = Physics2D.BoxCast(vector, numberRadius, 0, Vector2.up);
+        int counter = 20;
+        while (counter > 0 && hit.collider != null)
         {
             x = Random.Range(-10f, 10f);
             y = Random.Range(-5f, 5f);
             vector = new Vector3(x, y);
-            Debug.Log("aaaaaaaaaaaaaaaaa");
+            hit = Physics2D.BoxCast(vector, numberRadius, 0, Vector2.up);
+            counter--;
+            Debug.Log("aaaaaaaaaaaaaaaaaaaaa");
         }
+
+        if (counter == 0)
+            return Vector3.zero;
 
         return vector;
     }
@@ -60,7 +69,7 @@ public class GameController : MonoBehaviour
     void Start()
     {
         SpawnPlayer();
-        for (int i = 0; i < 15; i++)
+        for (int i = 0; i < 100; i++)
             SpawnNewNumber();
     }
 
