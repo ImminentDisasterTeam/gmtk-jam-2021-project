8674f336c41704e279abd86e6b64a507e8fd1c03
Pirtness <edelal60@mail.ru>
2021-06-12 14:54:44 +0300

    make numbers with digits and storage

--- Assets/Scripts/Digit.cs
+++ Assets/Scripts/Digit.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Digit : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public Action<GameObject> onCollisionEnter2D;
+
+    int value;
+    public int GetValue() {
+        return value;
+    }
+    
+    public void SetValue(int value) {
+        this.value = value;
+    }
+
     void Start()
+    {
+        var spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = Resources.LoadAll<Sprite>("Sprites/digits")[value];
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Enemy") {
+            onCollisionEnter2D(gameObject);
+        }
     }
 }
--- Assets/Scripts/Number.cs
+++ Assets/Scripts/Number.cs
@@ -1,38 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Number : MonoBehaviour
 {
-    List<Digit> digits;
+    List<GameObject> digits;
+    [SerializeField] int value;
+    
+    [SerializeField] GameObject digitPrefab;
+    [SerializeField] float digitWidth = 0.5f;
+
+    public Action<GameObject> onDestroy;
 
     public int GetValue()
     {
-        return 0;
+        return value;
     }
 
-    public void Destroy()
+    private void Destroy()
     {
+        // onDestroy(gameObject);
+        Destroy(gameObject);
+    }
 
+    private void DestroyDigits() {
+        foreach (var d in digits) {
+            Destroy(d);
+        }
+        digits = new List<GameObject>();
     }
 
     public static Number Summ(Number left, Number right)
     {
-        return null;
+        int sum = left.GetValue() + right.GetValue();
+        left.Initiate(sum);
+        Destroy(right.gameObject);
+        return left;
     }
 
     public void Initiate(int value)
     {
-
+        DestroyDigits();
+        this.value = value;
+        InstantiateDigits();
     }
 
-    // Start is called before the first frame update
+    private void InstantiateDigits() {
+        int v = value;
+        float offset = -digitWidth;
+        while (v > 0) {
+            Debug.Log(v%10);
+            GameObject go = Instantiate(digitPrefab, transform.position + new Vector3(offset, 0, 0), Quaternion.identity, transform);
+            go.GetComponent<Digit>().SetValue(v%10);
+            digits.Add(go);
+            go.GetComponent<Digit>().onCollisionEnter2D += obj => Destroy();
+            offset -= digitWidth;
+            v/=10;
+        }
+    }
+
+    public float GetWidth() {
+        return digitWidth*digits.Count;
+    }
+
+    public Vector2 GetBordersX() {
+        return new Vector2 (transform.position.x - GetWidth(), transform.position.x);
+    }
+
     void Start()
     {
-
+        digits = new List<GameObject>();
+        Initiate(value);
     }
 
-    // Update is called once per frame
     void Update()
     {
 
