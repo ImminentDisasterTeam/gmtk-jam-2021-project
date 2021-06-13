72c12098e3af501cd138230c79a2533429135259
AntonyMoes <32247865+AntonyMoes@users.noreply.github.com>
2021-06-13 18:11:56 +0300

    Fix camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,6 +9,7 @@ public class CameraController : MonoBehaviour
     [SerializeField] float xOffset = 1;
     [SerializeField] float yOffset = 1;
     [SerializeField] LevelSizeHolder levelSizeHolder;
+    Vector2 _levelSize;
 
     void Awake()
     {
@@ -16,12 +17,16 @@ public class CameraController : MonoBehaviour
         levelSizeHolder.OnChange += OnChangeSize;
     }
 
-    // Update is called once per frame
-    void OnChangeSize(Vector2 levelSize) {
-        var minWidth = levelSize.x / 2 + xOffset;
-        var minHeight = levelSize.y / 2 + yOffset;
+    void Start() {
+        var minWidth = _levelSize.x / 2 + xOffset;
+        var minHeight = _levelSize.y / 2 + yOffset;
 
         var minWidthInHeight = minWidth / _camera.aspect;
         _camera.orthographicSize = Math.Max(minWidthInHeight + xOffset, minHeight + yOffset);
+    }
+
+    // Update is called once per frame
+    void OnChangeSize(Vector2 levelSize) {
+        _levelSize = levelSize;
     }
 }
